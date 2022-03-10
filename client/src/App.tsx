26065f6e1680e3c9b8b98b50a5f31e284d90555a
paulusli <paulusli@tencent.com>
2022-03-10 20:51:08 +0800

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
+interface Todo {
+  id: number;
+  title: string;
+  status: 'todo' | 'done';
+}
+
+const http = axios.create({
+  baseURL: 'http://localhost:4200',
+})
+
+const App = () => {
+  const [newTodoTitle, setNewTodoTitle] = useState<string>('');
+  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  // 添加 todo
+  const addTodo = async () => {
+    await http.post('/todo', {
+      title: newTodoTitle,
+      status: 'todo',
+    })
+    await fetchTodoList();
+  }
+
+  // 获取访问量，并添加一个访问量
+  const fetchCount = async () => {
+    await http.post('/count');
+    const { data } = await http.get('/count');
+    setCount(data.myCount);
+  }
+
+  // 获取 todo 列表
+  const fetchTodoList = async () => {
+    const { data } = await http.get('/todo');
+    setTodoList(data.todoList);
+  }
+
+  useEffect(() => {
+    fetchCount().then();
+    fetchTodoList().then();
+  }, []);
+
+  return (
+    <div className="App">
+      <header>网站访问量：{count}</header>
+
+      <ul>
+        {todoList.map(todo => (
+          <li key={todo.id}>{todo.title} - {todo.status}</li>
+        ))}
+      </ul>
+
+      <div>
+        <input value={newTodoTitle} onChange={e => setNewTodoTitle(e.target.value)} type="text"/>
+        <button onClick={addTodo}>提交</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/routes/todo.js
@@ -0,0 +1,49 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const express = require("express");
+
+const router = express.Router();
+
+// 连接数据库
+const sequelize = new Sequelize({
+  host: process.env.NODE_ENV === 'docker' ? 'docker-todo-mariadb' : "127.0.0.1" ,
+  database: 'docker_todo',
+  username: 'root',
+  password: '123456',
+  dialect: 'mariadb',
+});
+
+// 定义 todo model
+const Todo = sequelize.define('Todo', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: { type: DataTypes.STRING },
+  status: { type: DataTypes.STRING }
+}, {});
+
+// 同步数据库结构
+sequelize.sync({ force: true }).then(() => {
+  console.log('已同步');
+});
+
+router.get('/', async (req, res) => {
+  // 获取 todo list
+  const todoList = await Todo.findAll();
+  res.json({ todoList });
+})
+
+router.post('/', async (req, res) => {
+  const { title, status } = req.body;
+
+  // 创建一个 todo
+  const newTodo = await Todo.create({
+    title,
+    status: status || 'todo',
+  });
+
+  res.json({ todo: newTodo })
+});
+
+module.exports = router;
